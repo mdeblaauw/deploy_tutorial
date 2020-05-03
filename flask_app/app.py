8b77c5a8bfb95b9a0ce73a3bf70af29bd0f5c993
mdeblaauw <mdeblaauw@mobiquityinc.com>
2020-05-03 20:38:46 +0200

    evening dump

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,6 @@
 import time
 from flask import Flask
+from flask import request
 
 server = Flask(__name__)
 
@@ -10,6 +11,12 @@ def hello_world():
 @server.route('/app')
 def hello_mark():
     return 'Hello Mark!!'
+
+@server.route('/requests')
+def print_requests():
+    request.get_json()
+    print(type(request))
+    return str(type(request))
 
 if __name__ == '__main__':
     server.run(debug=True, host='0.0.0.0', port=8000)
